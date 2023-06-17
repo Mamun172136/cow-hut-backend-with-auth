5eeddfa810b9d78023f31c09787409571218e530
Mamun172136 <ma1536204@gmail.com>
2023-06-17 23:07:08 +0600

    [done] checking Instanse of Error and custom ApiError in global Error Handler

--- src/app/middlewres/globalErrorHandler.ts
+++ src/app/middlewres/globalErrorHandler.ts
@@ -2,6 +2,7 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import handleValidationError from '../../errors/handleValidationerror'
+import ApiError from '../../errors/ApiError'
 
 //import ApiError from '../../errors/ApiError';
 
@@ -27,6 +28,27 @@ const globalErrorHandler: ErrorRequestHandler = (
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
   }
 
   res.status(statusCode).json({
