d5c0fc4ba89f201ad76e174a5b64be1b75f6a634
Mamun172136 <ma1536204@gmail.com>
2023-06-18 02:47:51 +0600

    [done] add unhandled Rejection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,28 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { Server } from 'http'
 async function main() {
+  let server: Server
   try {
     await mongoose.connect(config.database as string)
 
     console.log('database is connected successfully')
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
     console.log(' connect holo na ', error)
   }
+  process.on('unhandledRejection', err => {
+    if (server) {
+      server.close(() => {
+        console.log('unhandled rejection: closing our server', err)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 main()
